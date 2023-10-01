1d00a821bfe5314b085f5ba30f3393d9619a98a9
Fabio Angelici <fabio.angelici@outlook.it>
2023-10-01 13:04:09 +0200

    Modifiche minori

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,18 @@
-import Logo from '../Assets/images/logo.png';
+import Logo from '../assets/images/logo.png';
 
 export default function Navbar() {
-  return (
-    <header>
-      <nav className="shadow-lg">
-        <div className="min-h-[100px] bg-[#98AFBA] flex items-center justify-center p-5">
-          <div className="flex items-center">
-            <img className="h-[80px]" src={Logo} alt="" />
-            <p className="text-4xl font-electro text-white">Generatore di Password</p>
-          </div>
-        </div>
-      </nav>
-    </header>
-  );
+    return (
+        <header>
+            <nav className="shadow-lg">
+                <div className="flex min-h-[100px] items-center justify-center bg-[#98AFBA] p-5">
+                    <div className="flex items-center">
+                        <img className="h-[80px]" src={Logo} alt="" />
+                        <p className="font-electro text-4xl text-white">
+                            Generatore di Password
+                        </p>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
 }
