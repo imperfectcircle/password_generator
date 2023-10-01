67aab27ea91957f4d1ab5baf19d827ba50055152
Fabio Angelici <fabio.angelici@outlook.it>
2023-10-01 15:42:29 +0200

    Aggiunto analytics per vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { Analytics } from '@vercel/analytics/react';
 import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <HelmetProvider>
         <React.StrictMode>
             <App />
+            <Analytics />
         </React.StrictMode>
     </HelmetProvider>,
 );
